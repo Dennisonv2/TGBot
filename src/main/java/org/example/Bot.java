b61591550537826f740001d1d1385852c6c00fc2
Denis_Karimov <karimov.lololowka2017@ya.ru>
2024-02-24 09:51:10 +0500

    Initial commit

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    //создаем две константы, присваиваем им значения токена и имя бота соответсвтенно
+    final private String BOT_TOKEN = "6725473244:AAEsNLZdaAvnQ-UKWHVKhrCZH8LdWZL9FE4";
+    final private String BOT_NAME = "kre4evseh_bot";
+    Storage storage;
+    private ReplyKeyboardMarkup replyKeyboardMarkup;
+
+    Bot() {
+        storage = new Storage();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                //Извлекаем из объекта сообщение пользователя
+                Message inMess = update.getMessage();
+
+                //Достаем из inMess id чата пользователя
+                String chatId = inMess.getChatId().toString();
+
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                String response = parseMessage(inMess.getText());
+
+                //Создаем объект класса SendMessage - наш будущий ответ пользователю
+                SendMessage outMess = new SendMessage();
+
+                //Добавляем в наше сообщение id чата а также наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+
+                //Отправка в чат
+                execute(outMess);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String parseMessage(String textMsg) {
+        String response;
+
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if (textMsg.equals("/start"))
+            response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
+        else if (textMsg.equals("/get"))
+            response = storage.getRandQuote();
+        else if (textMsg.equals("Выдай базу"))
+            response = storage.getRandQuote();
+        else if (textMsg.equals("Просвяти"))
+            response = storage.getRandQuote();
+        else
+            response = "Сообщение не распознано";
+
+        return response;
+    }
+}
+
